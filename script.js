3ca639ec4f7b21a0cf55960435ff2c192fece036
Mudassir Noor <mnoor@sfu.ca>
2019-12-30 18:22:23 -0800

    added summary popup and refactored script code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const MISSPELLEDWORDCOUNT = document.querySelector("#misspelledWordCount");
 const WPM = document.querySelector("#wpm");
-const ORIGINALTEXT = document.querySelector("#originalText").innerHTML;
+const ORIGINALTEXT = document.querySelector("#originalText").innerText;
 const TEXTAREA = document.querySelector("textArea");
 const CLOCK = document.querySelector("#clock");
-const RESET = document.querySelector("button");
+const RESET = document.querySelector("#reset");
+const SUMMARY = document.querySelector(".summary");
+const CLOSESUMMARY = document.querySelector("#closeSummary");
 
 var clockInterval;
 var wpmInterval;
@@ -11,10 +13,12 @@ var timerRunning = false;
 var millisecondsCounter = 0;
 var secondsCounter = 0;
 var wpmSecondsCounter = 0;
+var wpm = 0;
+var misspelledWordsCount = 0;
 
 var originTextArray = ORIGINALTEXT.split(" ");
 
-const Clock = {
+var Clock = {
     minutes: 0,
     seconds: 0,
     updateTime: function () {
@@ -56,7 +60,7 @@ function wordsPerMinute() {
     let inputText = TEXTAREA.value;
     let minutesPassed = wpmSecondsCounter / 60;
     let wordsTyped = inputText.length / 5; //1 word counts as 5 characters
-    let wpm = Math.trunc(wordsTyped / minutesPassed);
+    wpm = Math.trunc(wordsTyped / minutesPassed);
 
     WPM.innerHTML = wpm;
     return wpm;
@@ -67,9 +71,8 @@ function spellCheck () {
     let originTextSubstring = ORIGINALTEXT.substring(0, inputText.length);
 
     if (inputText == ORIGINALTEXT) {
-        //TODO popup window showing final wpm and misspelled word count
-        resetTimer();
         TEXTAREA.style.borderColor = "#33FFD5";
+        summary();
     }
     else {
         if (inputText == originTextSubstring) {
@@ -83,51 +86,65 @@ function spellCheck () {
 
 //Function to find misspelled words count
 function misspelledWords (inputText) {
-    let misspelledWordCount = 0;
+    misspelledWordsCount = 0;
     let inputTextArray = inputText.split(" ");
     for (let i = 0; i < inputTextArray.length - 1; i++) {
         if (inputTextArray[i] != originTextArray[i]) {
-            misspelledWordCount++;
+            misspelledWordsCount++;
         }
     }
-    MISSPELLEDWORDCOUNT.innerHTML = addLeadingZeros(misspelledWordCount);
+    MISSPELLEDWORDCOUNT.innerHTML = addLeadingZeros(misspelledWordsCount);
 }
 
-
-
-//Reset timer and text area when the reset button is pressed
-function resetTimer () {
-    timerRunning = false;
-    clearInterval(clockInterval);
-    clockInterval = null;
-    Clock.minutes = 0;
-    Clock.seconds = 0;
-    millisecondsCounter = 0;
-
-    TEXTAREA.value = "";
-    TEXTAREA.style.borderColor = "#888";
-    CLOCK.innerHTML = Clock.printClock();
-    console.log("Reset button was pressed");
-}
-
-function resetAnalysis() {
-    MISSPELLEDWORDCOUNT.innerHTML = "00";
-    WPM.innerHTML = "00";
-}
-
-function resetCall () {
+//Function to open pop up and display test statistics
+function summary() {
     resetTimer();
-    resetAnalysis();
+    SUMMARY.querySelector("#statistics").innerHTML = "Misspelled words = " + misspelledWordsCount + " WPM = " + wpm;
+    SUMMARY.style.display = "block";
 }
+
 function callAnalysis () {
     let inputText = TEXTAREA.value;
     spellCheck();
     misspelledWords(inputText);
 }
 
+//Reset timer and text area when the reset button is pressed
+function resetTimer () {
+    timerRunning = false;
+    clearInterval(clockInterval);
+    clearInterval(wpmInterval);
+}
+
+//The following are functions to reset document and script properties
+function resetCall () {
+    resetTimer();
+    resetDocument();
+    resetAnalysis();
+}
+
+function resetAnalysis() {
+    wpm = 0;
+    misspelledWordsCount = 0;
+    MISSPELLEDWORDCOUNT.innerHTML = "00";
+    WPM.innerHTML = "00";
+}
+
+function resetDocument (){
+    clockInterval = null;
+    Clock.minutes = 0;
+    Clock.seconds = 0;
+    millisecondsCounter = 0;
+    TEXTAREA.value = "";
+    TEXTAREA.style.borderColor = "#888";
+    CLOCK.innerHTML = Clock.printClock();
+}
+
+CLOSESUMMARY.onclick = function () {
+    SUMMARY.style.display = "none";
+    resetCall();
+};
+
 TEXTAREA.addEventListener("keypress", startClock, false);
 TEXTAREA.addEventListener("keyup", callAnalysis, false);
 RESET.addEventListener("click", resetCall, false);
-
-
-//keypress event occurs when a key is pressed but no output is committed to the UI
